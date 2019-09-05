d04e3b5a2c89f3c75ab4a32e5ee12a89df9100ad
alittlecross <alittlecross@live.co.uk>
2019-09-05 11:00:24 +0100

    updated context colour logging functions to correctly position colon

--- src/js/messages.js
+++ src/js/messages.js
@@ -4,5 +4,5 @@ const blue = '\x1b[94m'
 const pink = '\x1b[95m'
 const reset = '\x1b[0m'
 
-module.exports.infoMsg = (prefix, content) => console.log(`${blue}:${prefix}${reset}${content}`)
-module.exports.warningMsg = (prefix, content) => console.log(`${pink}:${prefix}${reset}${content}`)
+module.exports.infoMsg = (prefix, content) => console.log(`${blue}${prefix}: ${reset}${content}`)
+module.exports.warningMsg = (prefix, content) => console.log(`${pink}${prefix}: ${reset}${content}`)
