53fb7696688581173aa3eb03a46d675361b2bc70
alittlecross <alittlecross@live.co.uk>
2019-09-05 10:07:16 +0100

    updated step-definitions so as not to require ordinals in quotes

--- base/src/features/X2_can_i_search_for_pensions_and_navigate_from_there.feature
+++ base/src/features/X2_can_i_search_for_pensions_and_navigate_from_there.feature
@@ -4,7 +4,7 @@ Feature: Can I search for 'pensions', and navigate from there?
 
   Scenario: Attempt to search for 'pensions'
     Given I visit the url "https://www.gov.uk" *
-    When I enter "pensions" in the "1st" search field *
+    When I enter "pensions" in the 1st search field *
     Then I click the "Search" button *
     Then I click the "Check your State Pension age" link *
     Then it has a page title "Check your State Pension age - GOV.UK" *
--- src/step-definitions/index.js
+++ src/step-definitions/index.js
@@ -26,23 +26,23 @@ defineStep('I enter {string} in the text field *', text => page.formField('text'
 
 defineStep('I enter {string} in the text area *', text => page.textArea(text))
 
-defineStep('I click the {string} {string} link *', (index, text) => page.clickLink(text, parseInt(index) - 1))
+defineStep('I click the {} {string} link *', (index, text) => page.clickLink(text, parseInt(index) - 1))
 
-defineStep('I click the {string} {string} button *', (index, text) => page.clickButton(text, parseInt(index) - 1))
+defineStep('I click the {} {string} button *', (index, text) => page.clickButton(text, parseInt(index) - 1))
 
-defineStep('I select the {string} answer {string} *', (index, text) => page.selectAnswer(text, parseInt(index) - 1))
+defineStep('I select the {} answer {string} *', (index, text) => page.selectAnswer(text, parseInt(index) - 1))
 
-defineStep('I select the {string} {string} option {string} *', (index, text, option) => page.selectOption(text, option, parseInt(index) - 1))
+defineStep('I select the {} {string} option {string} *', (index, text, option) => page.selectOption(text, option, parseInt(index) - 1))
 
-defineStep('I enter {string} in the {string} email field *', (text, index) => page.formField('email', text, parseInt(index) - 1))
+defineStep('I enter {string} in the {} email field *', (text, index) => page.formField('email', text, parseInt(index) - 1))
 
-defineStep('I enter {string} in the {string} password field *', (text, index) => page.formField('password', text, parseInt(index) - 1))
+defineStep('I enter {string} in the {} password field *', (text, index) => page.formField('password', text, parseInt(index) - 1))
 
-defineStep('I enter {string} in the {string} search field *', (text, index) => page.formField('search', text, parseInt(index) - 1))
+defineStep('I enter {string} in the {} search field *', (text, index) => page.formField('search', text, parseInt(index) - 1))
 
-defineStep('I enter {string} in the {string} text field *', (text, index) => page.formField('text', text, parseInt(index) - 1))
+defineStep('I enter {string} in the {} text field *', (text, index) => page.formField('text', text, parseInt(index) - 1))
 
-defineStep('I enter {string} in the {string} text area *', (text, index) => page.textArea(text, parseInt(index) - 1))
+defineStep('I enter {string} in the {} text area *', (text, index) => page.textArea(text, parseInt(index) - 1))
 
 defineStep('it has a page title {string} *', text => page.hasTitle(text))
 
