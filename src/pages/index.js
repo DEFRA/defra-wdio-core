8045dd6a10d31f615b1ff43f2b27cce80a3e06de
alittlecross <alittlecross@live.co.uk>
2019-09-12 08:32:12 +0100

    added additional functions to aid QA ways of working. sorted functions alphabetically for easier locating

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,28 @@
 
 const assert = require('assert')
 const ordinal = require('ordinal')
+
 const { infoMsg, warningMsg } = require('../js/messages')
 
 class Core {
-  _attibutesBuilder (attributes) {
-    const a = []
-    for (let i = 0; i < attributes.length; i += 2) a.push(`[${attributes[i]}='${attributes[i + 1]}']`)
-    return a.join('')
+  _attibutesBuilder (a) {
+    const attributes = []
+    for (let i = 0; i < a.length; i += 2) attributes.push(`[${a[i]}='${a[i + 1]}']`)
+    return attributes.join('')
+  }
+
+  _checkUrl (a) {
+    const waitforTimeout = browser.options.waitforTimeout
+    let b
+    try {
+      browser.waitUntil(() => {
+        b = browser.getUrl()
+        return b.includes(a)
+      }, waitforTimeout, `Expected Url ${b} to include ${a}`)
+    } catch (error) {
+      this.screenshot()
+      throw error
+    }
   }
 
   _checkUrlChange (a, b, expectUrlChange) {
@@ -20,14 +35,53 @@ class Core {
     }
   }
 
-  screenshot (location = './logs/error-screenshots/', prefix = 'error') {
-    const timestamp = new Date().toISOString().substring(0, 19)
-    const browserName = browser.capabilities.browserName.toLowerCase()
-    browser.saveScreenshot(`${location}${prefix}.${timestamp}.${browserName}.png`)
+  clear (type, ...attributes) {
+    this.get(`${type}${this._attibutesBuilder(attributes)}`).clearValue()
   }
 
-  wait (milliseconds) {
-    browser.pause(milliseconds)
+  checkUrl (url) {
+    const waitforTimeout = browser.options.waitforTimeout
+    let browserUrl
+    try {
+      browser.waitUntil(() => {
+        browserUrl = browser.getUrl()
+        return browserUrl.includes(url)
+      }, waitforTimeout, `Expected Url ${browserUrl} to include ${url}`)
+    } catch (error) {
+      this.screenshot()
+      throw error
+    }
+  }
+
+  click (type, text = null, expectUrlChange = false, ...attributes) {
+    const a = browser.getUrl()
+    this.get(`${type}${this._attibutesBuilder(attributes)}`, text).click()
+    const b = browser.getUrl()
+    this._checkUrlChange(a, b, expectUrlChange)
+  }
+
+  clickButton (selector, expectUrlChange = true) {
+    this.click(selector, null, expectUrlChange)
+  }
+
+  clickButtonByText (text, expectUrlChange = true) {
+    this.click('button', text, expectUrlChange)
+  }
+
+  clickLabelByText (text) {
+    this.click('label', text)
+  }
+
+  clickLink (selector, expectUrlChange = true) {
+    this.click(selector, null, expectUrlChange)
+  }
+
+  clickLinkByText (text, expectUrlChange = true) {
+    this.click('a', text, expectUrlChange)
+  }
+
+  enter (type, text, ...attributes) {
+    this.set(`${type}${this._attibutesBuilder(attributes)}`, text)
   }
 
   get (selector, text = null, milliseconds = 0, log = true, index = 0) {
@@ -60,48 +114,34 @@ class Core {
     }
   }
 
+  hasElement (type, text, milliseconds = 500, ...attributes) {
+    const a = this.get(`${type}${this._attibutesBuilder(attributes)}`, null, milliseconds).getText()
+    assert.strictEqual(a, text)
+  }
+
+  hasText (text, milliseconds = 500) {
+    const a = this.get('*', null, milliseconds, false).getHTML().includes(text)
+    if (!a) this.screenshot()
+    assert.strictEqual(a, true, `Element with content '${text}' not found.`)
+  }
+
+  hasTitle (text, milliseconds = 500) {
+    const a = this.get('title', null, milliseconds).getTitle()
+    assert.strictEqual(a, text)
+  }
+
   set (selector, text) {
     this.get(selector).setValue(text)
   }
 
-  enter (type, text, ...attributes) {
-    this.set(`${type}${this._attibutesBuilder(attributes)}`, text)
+  screenshot (location = './logs/error-screenshots/', prefix = 'error') {
+    const timestamp = new Date().toISOString().substring(0, 19)
+    const browserName = browser.capabilities.browserName.toLowerCase()
+    browser.saveScreenshot(`${location}${prefix}.${timestamp}.${browserName}.png`)
   }
 
-  visit (text, expectUrlChange = true, milliseconds = 0) {
-    this.wait(milliseconds)
-
-    const a = browser.getUrl()
-    browser.url(text)
-    const b = browser.getUrl()
-    this._checkUrlChange(a, b, expectUrlChange)
-
-    if (text !== b) {
-      warningMsg('WARNING', `Url redirected from '${text}' to '${b}'`)
-    }
-  }
-
-  click (type, text = null, expectUrlChange = false, ...attributes) {
-    const a = browser.getUrl()
-    this.get(`${type}${this._attibutesBuilder(attributes)}`, text).click()
-    const b = browser.getUrl()
-    this._checkUrlChange(a, b, expectUrlChange)
-  }
-
-  clickButton (text, expectUrlChange = true) {
-    this.click('button', text, expectUrlChange)
-  }
-
-  clickLabel (text) {
-    this.click('label', text)
-  }
-
-  clickLink (text, expectUrlChange = true) {
-    this.click('a', text, expectUrlChange)
-  }
-
-  select (option, ...attributes) {
-    this.get(`select${this._attibutesBuilder(attributes)}`).selectByVisibleText(option)
+  select (type, option, ...attributes) {
+    this.get(`${type}${this._attibutesBuilder(attributes)}`).selectByVisibleText(option)
   }
 
   selectByLabel (text, option) {
@@ -115,20 +155,21 @@ class Core {
     this.selectByLabel('Year', a[2])
   }
 
-  hasTitle (text, milliseconds = 500) {
-    const a = this.get('title', null, milliseconds).getTitle()
-    assert.strictEqual(a, text)
+  visit (text, expectUrlChange = true, milliseconds = 0) {
+    this.wait(milliseconds)
+
+    const a = browser.getUrl()
+    browser.url(text)
+    const b = browser.getUrl()
+    this._checkUrlChange(a, b, expectUrlChange)
+
+    if (!b.includes(text)) {
+      warningMsg('WARNING', `Url redirected from '${text}' to '${b}'`)
+    }
   }
 
-  hasText (text, milliseconds = 500) {
-    const a = this.get('*', null, milliseconds, false).getHTML().includes(text)
-    if (!a) this.screenshot()
-    assert.strictEqual(a, true, `Element with content '${text}' not found.`)
-  }
-
-  hasElement (type, text, milliseconds = 500, ...attributes) {
-    const a = this.get(`${type}${this._attibutesBuilder(attributes)}`, text, milliseconds).getText()
-    assert.strictEqual(a, text)
+  wait (milliseconds) {
+    browser.pause(milliseconds)
   }
 }
 
