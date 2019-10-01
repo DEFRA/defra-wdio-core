e012245ebeddb54d54cd3b1ba6f6618cd11cf6dc
Paul Fazackerley <alittlecross@live.co.uk>
2019-10-01 16:50:45 +0100

    updated to match revisions to core functions

--- base/src/specs/X1_can_i_use_the_check_your_state_pension_age_tool.js
+++ base/src/specs/X1_can_i_use_the_check_your_state_pension_age_tool.js
@@ -8,23 +8,23 @@ const page = require('../pages/example')
 describe(`Can I use the 'Check your State Pension age' tool?`, () => {
   it(`it has "You’ll reach State Pension age on 29 September 2051." on the page`, () => {
     page.visit('state-pension-age')
-    core.clickLink('Start now')
-    core.clickLabel('State Pension age - including Pension Credit qualifying age')
-    core.clickButton('Next step')
+    core.clickLinkByText('Start now')
+    core.clickLabelByText('State Pension age - including Pension Credit qualifying age')
+    core.clickButtonByText('Next step')
 
     /** DO THIS **/
 
-    // core.selectOption('Day', '29')
-    // core.selectOption('Month', 'September')
-    // core.selectOption('Year', '1983')
+    // core.selectByLabel('Day', '29')
+    // core.selectByLabel('Month', 'September')
+    // core.selectByLabel('Year', '1983')
 
     /** OR DO THIS **/
 
     core.selectDob('29 September 1983')
 
-    core.clickButton('Next step')
-    core.clickLabel('Man')
-    core.clickButton('Next step')
+    core.clickButtonByText('Next step')
+    core.clickLabelByText('Man')
+    core.clickButtonByText('Next step')
 
     /** DO THIS **/
 
