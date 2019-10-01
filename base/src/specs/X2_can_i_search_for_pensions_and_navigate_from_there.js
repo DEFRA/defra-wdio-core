4cd30bcea48593f4fc17350d62e05c8c38d0981b
Paul Fazackerley <alittlecross@live.co.uk>
2019-10-01 16:45:44 +0100

    updated to match revisions to core functions

--- base/src/specs/X2_can_i_search_for_pensions_and_navigate_from_there.js
+++ base/src/specs/X2_can_i_search_for_pensions_and_navigate_from_there.js
@@ -8,8 +8,8 @@ describe(`Can I search for 'pensions', and navigate from there?`, () => {
   it(`it has a page title "Check your State Pension age - GOV.UK"`, () => {
     core.visit('https://www.gov.uk/')
     core.enter('input', 'pensions', 'title', 'Search')
-    core.clickButton('Search')
-    core.clickLink('Check your State Pension age')
+    core.clickButtonByText('Search')
+    core.clickLinkByText('Check your State Pension age')
     core.hasTitle('Check your State Pension age - GOV.UK')
   })
 })
