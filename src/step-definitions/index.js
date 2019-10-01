6cb1deb06afe26fac244449708e6a004438e9540
Paul Fazackerley <alittlecross@live.co.uk>
2019-10-01 16:56:19 +0100

    updated to match revisions to core functions

--- src/step-definitions/index.js
+++ src/step-definitions/index.js
@@ -4,15 +4,15 @@ const { defineStep } = require('cucumber')
 const Core = require('../../src/pages')
 const core = new Core()
 
-defineStep('I click the {string} button *', text => core.clickButton(text))
+defineStep('I click the {string} button *', text => core.clickButtonByText(text))
 
-defineStep('I click the {string} link *', text => core.clickLink(text))
+defineStep('I click the {string} link *', text => core.clickLinkByText(text))
 
 defineStep('I enter {string} in the {string} with the {string} of {string} *', (text, type, attribute, content) => core.enter(type, text, attribute, content))
 
 defineStep('I select the {string} option {string} *', (text, option) => core.selectByLabel(text, option))
 
-defineStep('I select the answer {string} *', text => core.clickLabel(text))
+defineStep('I select the answer {string} *', text => core.clickLabelByText(text))
 
 defineStep('I select the dob {string} *', text => core.selectDob(text))
 
